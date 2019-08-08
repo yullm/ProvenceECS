4a2eafacbb2511ab6ae2b68b810126e3cf90d98f
yullm <yull.michael14@gmail.com>
2019-08-07 20:18:08 -0700

    Added System Editor

---
+++ Assets/Editor/SystemManagerEditor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace ProvenceECS{
+
+    [CustomEditor(typeof(SystemManager))]
+    public class SystemManagerEditor : Editor
+    {
+        public override void OnInspectorGUI(){
+            DrawDefaultInspector();
+        }
+    }
+}
