656ae4c30888a1935500954db51b5a9ef769e0cc
Michael Yull <yull.michael14@gmail.com>
2023-09-12 16:51:22 -0400

    Extension Addition

--- _ProvenceECS/_Core/ProvenceExtensions.cs
+++ _ProvenceECS/_Core/ProvenceExtensions.cs
@@ -299,6 +299,14 @@ namespace ProvenceECS{
                 world.RemoveEntity(entity);
             }
         }
+
+        public static Vector3 Snap(this Vector3 vector3, float gridSize = 1.0f){
+            return new Vector3(
+                Mathf.Round(vector3.x / gridSize) * gridSize,
+                Mathf.Round(vector3.y / gridSize) * gridSize,
+                Mathf.Round(vector3.z / gridSize) * gridSize
+            );
+        }
     }
 
 }
